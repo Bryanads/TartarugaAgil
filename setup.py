c8a3ae7c9c5367e58c384b755243162232217a50
marceufilho <filhomarceu@gmail.com>
2024-05-20 22:53:12 -0300

    created file structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+
+setup(
+    name="tartaruga",
+    version="0.1.0",
+    description="Managing tool for SCRUM teams",
+    author="Tartaruga Agil",
+    packages=find_packages(),
+)
